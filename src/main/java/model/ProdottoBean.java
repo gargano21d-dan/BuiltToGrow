143e5074ec3629dd35d7ae3ca321bba0abd420a9
Mario Boccia <bocciamario62@gmail.com>
2026-06-29 11:39:57 +0200

    bean Prodotto

---
+++ src/main/java/model/ProdottoBean.java
@@ -0,0 +1,59 @@
+package model;
+
+public class ProdottoBean {
+
+	private int id;
+	private String nome;
+	private String descrizione;
+	private double prezzo;
+	private int quantita;
+	private int categoriaId;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescrizione() {
+		return descrizione;
+	}
+
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+
+	public double getPrezzo() {
+		return prezzo;
+	}
+
+	public void setPrezzo(double prezzo) {
+		this.prezzo = prezzo;
+	}
+
+	public int getQuantita() {
+		return quantita;
+	}
+
+	public void setQuantita(int quantita) {
+		this.quantita = quantita;
+	}
+
+	public int getCategoriaId() {
+		return categoriaId;
+	}
+
+	public void setCategoriaId(int categoriaId) {
+		this.categoriaId = categoriaId;
+	}
+}
