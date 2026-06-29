6bd1f9a3d882ef238fa3a317ebe89c9b48359369
Mario Boccia <bocciamario62@gmail.com>
2026-06-29 22:13:51 +0200

    servlet admin

---
+++ src/main/java/control/admin/AdminControl.java
@@ -0,0 +1,40 @@
+package control.admin;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.ProdottoDAO;
+import model.ProdottoBean;
+import model.UtenteBean;
+
+@WebServlet("/admin")
+public class AdminControl extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		UtenteBean utente = (UtenteBean) request.getSession().getAttribute("utente");
+		if (utente == null || !"admin".equals(utente.getRuolo())) {
+			response.sendRedirect(request.getContextPath() + "/login");
+			return;
+		}
+
+		try {
+			List<ProdottoBean> prodotti = new ProdottoDAO().doRetrieveAll();
+			request.setAttribute("prodotti", prodotti);
+		} catch (SQLException e) {
+			throw new ServletException("Errore nel recupero dei prodotti", e);
+		}
+
+		request.getRequestDispatcher("/WEB-INF/views/admin/dashboard.jsp").forward(request, response);
+	}
+}
